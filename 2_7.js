b90eab1d501d527e4a131281e336e94a756bb0db
textoexpositivo <helenapc2018@gmail..com>
2020-12-05 20:48:35 -0300

    ver

--- 2_7.js
+++ 2_7.js
@@ -100,7 +100,7 @@ item('barExport', 'arrow-up-circle-outline', 'Crear copia de Seguridad')
 item('barImport', 'arrow-down-circle-outline', 'Cargar copia de Seguridad');
 item('barLogout', 'log-out-outline', 'Cerrar Sesión');
 const ver = document.createElement('ion-item-divider');
-setAttributes(ver, { innerHTML: 'Versión 2.7.099-beta-test' });
+ver.innerHTML = 'Versión 2.7.12-beta-test';
 barContent.appendChild(ver);
 item('barDelAcc', 'close-outline', 'Eliminar Cuenta', 'danger');
 
