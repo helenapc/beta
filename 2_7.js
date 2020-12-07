79568681faef21e5fc2ecfd1237c40511a2d9957
textoexpositivo <helenapc2018@gmail..com>
2020-12-07 11:12:11 -0300

    portail

--- 2_7.js
+++ 2_7.js
@@ -45,10 +45,8 @@ const buttonFocus = document.getElementById('buttonFocus');
 const content = document.getElementById('content');
 //******************************************* */
 
-
+screen.lockOrientation('portrait');
 document.getElementById('title').setAttribute('style', 'margin-left:38px');
-// document.getElementById('buttonAdd').setAttribute('style', 'opacity:0; margin-bottom:-200px');
-
 document.getElementById('nameSetting').setAttribute('style', 'pointer-events: none; opacity: 0');
 
 document.getElementById('buttonEdit').setAttribute('style', 'opacity:0; pointer-events: none');
@@ -107,7 +105,7 @@ item('barExport', 'arrow-up-circle-outline', 'Crear copia de Seguridad')
 item('barImport', 'arrow-down-circle-outline', 'Cargar copia de Seguridad');
 item('barLogout', 'log-out-outline', 'Cerrar Sesión');
 const ver = document.createElement('ion-item-divider');
-ver.innerHTML = 'Versión 2.7.19-beta-(active_anim)';
+ver.innerHTML = 'Versión 2.7.19-beta-(portail)';
 barContent.appendChild(ver);
 item('barDelAcc', 'close-outline', 'Eliminar Cuenta', 'danger');
 
